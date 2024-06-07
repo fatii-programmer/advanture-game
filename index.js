2ee8d0e2e87defcd036b5825799b13830d02cad7
fatii-programmer <160323457+fatii-programmer@users.noreply.github.com>
2024-06-07 14:48:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,161 @@
+#! /user/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+// classes Player & Opponent
+class Player {
+    name;
+    ;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+    fuelIncrease() {
+        this.fuel = 100;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+}
+// Player Name & Opponent Select
+let player = await inquirer.prompt({
+    type: "input",
+    name: "name",
+    message: "Please Enter Your Name:"
+});
+let opponent = await inquirer.prompt({
+    type: "list",
+    name: "select",
+    message: "Select Your  Opponent:",
+    choices: ["skeleton", "Assassin", "Zomebie"]
+});
+// Gather Data
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+do {
+    // attack
+    if (opponent.select == "skeleton") {
+        let ask = await inquirer.prompt({
+            type: "list",
+            name: "opt",
+            message: "Select Your  Opponent:",
+            choices: ["Attack", "Drink Portion", "Run For Your Life"]
+        });
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion Youer fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+    // assassin
+    if (opponent.select == "Assassin") {
+        let ask = await inquirer.prompt({
+            type: "list",
+            name: "opt",
+            message: "Select Your  Opponent:",
+            choices: ["Attack", "Drink Portion", "Run For Your Life"]
+        });
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion Youer fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+    // zombie
+    if (opponent.select == "Zomebie") {
+        let ask = await inquirer.prompt({
+            type: "list",
+            name: "opt",
+            message: "Select Your  Opponent:",
+            choices: ["Attack", "Drink Portion", "Run For Your Life"]
+        });
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p1.fuelDecrease();
+                console.log(`${chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`)} `);
+                console.log(`${chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`)} `);
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion Youer fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+} while (true);
